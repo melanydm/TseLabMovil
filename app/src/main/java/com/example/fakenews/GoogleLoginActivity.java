7ea2a025f5bb0f18a6e3d8e33815513c45067160
Melany Dolgay Martinez <melany.dolgay@fing.edu.uy>
2019-06-09 23:46:26 -0300

    error 400 en login request

--- app/src/main/java/com/example/fakenews/GoogleLoginActivity.java
+++ app/src/main/java/com/example/fakenews/GoogleLoginActivity.java
@@ -1,61 +1,55 @@
 package com.example.fakenews;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.accounts.Account;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Switch;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fakenews.apiWeb.ApiClient;
+import com.example.fakenews.apiWeb.ApiInterface;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class GoogleLoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener, GoogleApiClient.ConnectionCallbacks {
 
     GoogleApiClient mGoogleApiClient;
     private static final int RC_SIGN_IN = 9001;
-    private static final String TAG = "SignInActivity";
+    private static final String TAG = "SignInActivity >>>>>>>>> ";
     SignInButton signInButton;
     TextView statusTextView;
-    private FirebaseAuth mAuth;
     GoogleSignInClient mGoogleSignInClient;
-    private GoogleSignInAccount ac;
+    String url = "http://r179-27-99-70.ir-static.anteldata.net.uy:8080/FakeNews-web/RESTServices/citizen/";
+    private ApiInterface restApi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_login);
-        mAuth = FirebaseAuth.getInstance();
+        restApi = ApiClient.getClient(url).create(ApiInterface.class);
 
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-               // .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken(getString(R.string.server_client_id))
                 .requestEmail()
                 .build();
 
@@ -64,7 +58,8 @@ public class GoogleLoginActivity extends AppCompatActivity implements GoogleApiC
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this, this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
+                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+                .build();
 
         signInButton = findViewById(R.id.sign_in_button);
         statusTextView = findViewById(R.id.textView);
@@ -72,20 +67,38 @@ public class GoogleLoginActivity extends AppCompatActivity implements GoogleApiC
 
     } // onCreate
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+        //  Toast.makeText(getApplicationContext(),"Now onStart() calls", Toast.LENGTH_LONG).show(); //onStart Called
+
+        // GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        //  updateUI(account);
+
+
+    }
+
+
+
     public void onClick(View v){
         switch (v.getId()){
             case R.id.sign_in_button:
                 signIn();
-            break;
+                break;
         }
     }
 
     private void signIn() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+/*
+        String email = account.getEmail();
+        String tokenId = account.getIdToken();
+        requestLogin(email, tokenId);
+*/
+
+
         startActivityForResult(signInIntent, RC_SIGN_IN);
-        /*Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);*/
-        //startActivity(signInIntent, com.example.fakenews.MainActivity.class);
+
     }
 
     @Override
@@ -94,71 +107,99 @@ public class GoogleLoginActivity extends AppCompatActivity implements GoogleApiC
 
         //if (requestCode == RC_SIGN_IN){
         if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
         }
 
     }
-/*
-    private void handleSignInResult(Task<GoogleSignInAccount> result){
-        Log.d(TAG,"handleSignInResult:" + result.isSuccessful());
-
-        if (result.isSuccessful()) {
-            Account acct = result.getResult().getAccount();
-            // String str = "Hola, " + acct.getDisplayName();
-            statusTextView.setText("Hola");
-        } else {
-            statusTextView.setText("Sign in failed");
-        }
-    }*/
 
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-       try {
-           GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-           Log.w(TAG, "signInResult: ok=" + account.getDisplayName());
-           //Signed in successfully, show authenticated UI.
-           // updateUI(account);
-       } catch (ApiException e) {
-           // The ApiException status code indicates the detailed failure reason.
-           // Please refer to the GoogleSignInStatusCodes class reference for more information.
-           Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-           // updateUI(null);
-       }
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Log.w(TAG, "signInResult: ok=" + account.getDisplayName());
+            String idToken = account.getIdToken();
+            String mail = account.getEmail();
+            sendIdTokenMail(idToken, mail);
+            //
+            // updateUI(account);
+            Intent intent = new Intent(this, com.example.fakenews.MainActivity.class);
+            startActivity(intent);
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            // updateUI(null);
+        }
     }
 
+    private void sendIdTokenMail(String token, final String mail){
+        String body = mail + ", " + token; //new LoginBody(mail, token)
+        JSONObject jsonObj = new JSONObject();
+
+        try {
+            jsonObj.put("email", mail);
+            jsonObj.put("token_id", token);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Call<String> loginCall = restApi.login(jsonObj);
+        // Log.w(TAG, "lalalala, por encolar el pedido" + restApi.toString());
+        loginCall.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse
+                    (Call<String> call, Response<String> response) {
+
+                // Ocultar la barra de progreso
+                // showProgress(false);
+
+                if (response.isSuccessful()){
+                    Log.w(TAG, "login response successful");
+                    if (response.body() != null){
+                        if (!(response.body().contains("Error"))){
+
+                            // Guardar token de sesion y userID en preferencias
+                            //SessionPrefs.get(GoogleLoginActivity.this).guardarToken(response.body(), mail);
+                            Log.w(TAG, "response = " + response.body());
+                            // Ir al menu principal (main activity)
+
+                        }
+                        else {
+                            String error = response.body().toString();
+                            // showLoginError(error);
+                            Log.w(TAG, "response error del ojete: " + error);
+                        }
+
+                    }else{
+
+                        String error = "Error: Usuario o contraseña incorrecta";
+                        // showLoginError(error);
+                        Log.w(TAG, "response error 2" + error);
+                    }
+                }
+                // Procesar errores
+                else {
+
+                    String error = response.body();
+                    Log.w(TAG, "response error: " + error ); //+ " " +
+                    Log.w(TAG, "error" + response.code() + response.errorBody());
+                            //showLoginError(error);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                /*showProgress(false);
+                showLoginError(t.getMessage());*/
+                Log.w(TAG, "response error on failure: " + t + t.getCause());
+            }
+        });
+
+    }
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult){
         Log.d(TAG, "onConnectionFailed: " + connectionResult);
-    }
-
-    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
-
-        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            //updateUI(user);
-                            statusTextView.setText("Hola");
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            //Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
-                            //updateUI(null);
-                            statusTextView.setText("Sign in failed");
-                        }
-
-                        // ...
-                    }
-                });
     }
 
 
@@ -171,4 +212,6 @@ public class GoogleLoginActivity extends AppCompatActivity implements GoogleApiC
     public void onConnectionSuspended(int i) {
 
     }
+
+
 }
--- app/src/main/java/com/example/fakenews/MainActivity.java
+++ app/src/main/java/com/example/fakenews/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.fakenews;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fakenews.prefs.SessionPrefs;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Verifico sesión iniciada
+        if (!SessionPrefs.get(this).isLoggedIn()) {
+            startActivity(new Intent(this, GoogleLoginActivity.class));
+            finish();
+            return;
+        }
         setContentView(R.layout.activity_main);
         //setContentView(R.layout.activity_login);
 
@@ -26,11 +29,19 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         // Do something in response to button / Cambio displayMessageActivity por loginActivity
-        // Intent intent = new Intent(this, com.example.fakenews.DisplayMessageActivity.class);
+        // Intent intent = new Intent(this, co..............m.example.fakenews.DisplayMessageActivity.class);
         Intent intent = new Intent(this, com.example.fakenews.DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
+        /*EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
+        RequestQueue requestQueue;
+
+        //Llamado al WS
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+        Network network = new BasicNetwork(new HurlStack());
+        requestQueue = new RequestQueue(cache, network);
+        requestQueue.start();<
         //intent.putExtra(EXTRA_MESSAGE, message);
+*/
         startActivity(intent);
     }
 }
